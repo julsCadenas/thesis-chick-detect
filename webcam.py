88ed5fc26aa44cc58a4ecba98b738425c8442150
julsCadenas <jscadenas12@gmail.com>
2024-09-17 13:45:07 +0800

    trying video stream output

--- webcam.py
+++ webcam.py
@@ -2,27 +2,25 @@ import os
 from ultralytics import YOLO
 import cv2
 
-cap = cv2.VideoCapture(0)
+stream = 'http://192.168.1.2:8080/video'
+cap = cv2.VideoCapture(stream)
 
 if not cap.isOpened():
-    print("Error: Could not open webcam.")
+    print("Error: Could not open video stream.")
     exit()
 
 ret, frame = cap.read()
 if not ret:
-    print("Error: Could not read from webcam.")
+    print("Error: Could not read from video stream.")
     cap.release()
     exit()
 
 H, W, _ = frame.shape
 
-# Save the output video
 video_path_out = 'webcam_output.mp4'
 out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'mp4v'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
 
-# model_path = "C:/Users/Juls/Desktop/chicken/etian-last4.pt"
-# model_path = "C:/Users/Juls/Desktop/chicken/etian-best4.pt"
-model_path = "C:/Users/Juls/runs/detect/train25/weights/last.pt" # palitan mo ng path sa model mo
+model_path = "C:/Users/Juls/Desktop/models/etian-last4.pt"
 if not os.path.exists(model_path):
     print(f"Error: Model file not found at {model_path}")
     exit()
@@ -31,7 +29,12 @@ model = YOLO(model_path)
 
 threshold = 0.5
 
-while ret:
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Error: Could not read frame from video stream.")
+        break
+
     results = model(frame)[0]
 
     for result in results.boxes.data.tolist():
@@ -39,15 +42,13 @@ while ret:
 
         if score > threshold:
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
-            label = f"{results.names[int(class_id)].upper()} {score * 100:.2f}%" #eto yung percentage ng detection
-            cv2.putText(frame, label, (int(x1), int(y1 - 10)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2, cv2.LINE_AA)
+            label = f"{results.names[int(class_id)].upper()} {score * 100:.2f}%"
+            cv2.putText(frame, label, (int(x1), int(y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2, cv2.LINE_AA)
 
     out.write(frame)
     cv2.imshow('YOLO Webcam', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    ret, frame = cap.read()
 
 cap.release()
 out.release()
